1c1b7edd59990acf152e9588e54268a4c46b58fd
eivarin <rodrigommpereira309@gmail.com>
2025-02-05 02:30:42 +0000

    feat(sxhkd): add hotkeys to nix configuration

--- modules/home-manager/sxhkd/default.nix
+++ modules/home-manager/sxhkd/default.nix
@@ -17,6 +17,14 @@ in {
                 "super + d"             = "rofi -modi drun,run -show drun -show-icons -theme ~/.config/rofi/config.rasi";
                 "super + {t,shift + t,s,f}" = "bspc node -t {\~tiled,\~pseudo_tiled,\~floating,\~fullscreen}";
                 "super + shift + Escape" = "pkill -USR1 -x sxhkd";
+                "super + {_,shift + } {F1,F2,F3,Return}" = "toggle={true,false}; \
+                                                            {app=firefox ; icon=󰈹 ;\
+                                                            , app=code ; icon=󰨞 ;\
+                                                            , app=qtfm ; icon= ;\
+                                                            , app=kitty ; icon= ; \
+                                                            } \
+                                                            [[ "$toggle" = true ]] || (NNAME=$(rofi -dmenu -p "Name" -theme ~/.config/rofi/config.rasi); bspc monitor -a "$icon-$NNAME" && bspc desktop -f "$icon-$NNAME") ;\
+                                                            $app"
             };
         };
     };
