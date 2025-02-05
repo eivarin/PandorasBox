e79b08e12d2f59dd893edce7ffb4792f03171e1c
eivarin <rodrigommpereira309@gmail.com>
2025-02-05 02:23:28 +0000

    feat: config bspwm

--- modules/home-manager/bspwm/default.nix
+++ modules/home-manager/bspwm/default.nix
@@ -4,11 +4,34 @@ with lib;
 let cfg = config.modules.bspwm;
 
 in {
-    options.modules.bspwm = { enable = mkEnableOption "bspwm"; };
-    config = mkIf cfg.enable {
-      xsession.windowManager.bspwm = {
-        enable = true;
-        package = pkgs.bspwm;
+  options.modules.bspwm = { enable = mkEnableOption "bspwm"; };
+  config = mkIf cfg.enable {
+    xsession.windowManager.bspwm = {
+      enable = true;
+      package = pkgs.bspwm;
+      monitors = {
+        eDP-1 = [
+          "󰣇" 
+          "󰙯"
+          ""
+        ];
+      };
+      settings = {        
+        focus_follows_pointer     = true;
+        pointer_follows_monitor   = true;
+        border_width              = 2;
+        focused_border_color      = "#2a9dfb";
+        window_gap                = 10;
+        top_padding               = 25;
+        split_ratio               = 0.5;
+        borderless_monocle        = false;
+        gapless_monocle           = true;
+        single_monocle            = true;
+        top_monocle_padding       = 10;
+        pointer_modifier          = "mod4";
+        pointer_action1           = "move";
+        pointer_action2           = "resize_corner";
       };
     };
+  };
 }
