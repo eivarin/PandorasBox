98edca4ec5f3d20efb5973cd747fa5483867fde2
eivarin <rodrigommpereira309@gmail.com>
2025-08-04 13:58:18 +0100

    style: format wofi configuration for improved readability

--- home/grk/features/desktop/wofi.nix
+++ home/grk/features/desktop/wofi.nix
@@ -1,60 +1,60 @@
-{  lib, config, ... }:
+{ ... }:
 
 {
   programs.wofi = {
     enable = true;
     style = ''
-window {
-margin: 0px;
-border: 1px solid #bd93f9;
-background-color: #282a36;
-}
+      window {
+      margin: 0px;
+      border: 1px solid #bd93f9;
+      background-color: #282a36;
+      }
 
-#input {
-margin: 5px;
-border: none;
-color: #f8f8f2;
-background-color: #44475a;
-}
+      #input {
+      margin: 5px;
+      border: none;
+      color: #f8f8f2;
+      background-color: #44475a;
+      }
 
-#inner-box {
-margin: 5px;
-border: none;
-background-color: #282a36;
-}
+      #inner-box {
+      margin: 5px;
+      border: none;
+      background-color: #282a36;
+      }
 
-#outer-box {
-margin: 5px;
-border: none;
-background-color: #282a36;
-}
+      #outer-box {
+      margin: 5px;
+      border: none;
+      background-color: #282a36;
+      }
 
-#scroll {
-margin: 0px;
-border: none;
-}
+      #scroll {
+      margin: 0px;
+      border: none;
+      }
 
-#text {
-margin: 5px;
-border: none;
-color: #f8f8f2;
-} 
+      #text {
+      margin: 5px;
+      border: none;
+      color: #f8f8f2;
+      } 
 
-#entry.activatable #text {
-color: #282a36;
-}
+      #entry.activatable #text {
+      color: #282a36;
+      }
 
-#entry > * {
-color: #f8f8f2;
-}
+      #entry > * {
+      color: #f8f8f2;
+      }
 
-#entry:selected {
-background-color: #44475a;
-}
+      #entry:selected {
+      background-color: #44475a;
+      }
 
-#entry:selected #text {
-font-weight: bold;
-}
+      #entry:selected #text {
+      font-weight: bold;
+      }
     '';
   };
 }
